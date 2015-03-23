6486f1d8b688f4df66a08986697cbf8cc9bee783
rofrischmann <robin@rofrischmann.de>
2015-03-23 09:34:43 +0100

    OB-20 restructured and seperated core, custom and themes

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,26 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: '<json:package.json>',
         sass: {
-            dist: {
+            core: {
                 options: {
                     style: "compressed"
                 },
                 files: {
-                    'bin/obscene.min.css': 'source/obscene.scss'
+                    'bin/core.min.css': 'core/core.scss'
+                }
+            },
+            obscene: {
+                options: {
+                    style: "compressed"
+                },
+                files: {
+                    'bin/obscene.min.css': 'themes/obscene/obscene.scss'
                 }
             }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['sass:obscene']);
+    grunt.registerTask('obscene', ['sass:obscene']);
+    grunt.registerTask('core', ['sass:core']);
 };
